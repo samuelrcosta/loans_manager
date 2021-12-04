771fdb248c0777458fb94f089a853aea0af3155b
Samuel Costa <samu.rcosta@gmail.com>
2021-12-04 20:46:57 -0300

    commit for testing pipe

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,23 +3,8 @@
         <nav class="float-left">
             <ul>
                 <li>
-                    <a href="https://www.creative-tim.com">
-                        Creative Tim
-                    </a>
-                </li>
-                <li>
-                    <a href="https://creative-tim.com/presentation">
-                        About Us
-                    </a>
-                </li>
-                <li>
-                    <a href="http://blog.creative-tim.com">
-                        Blog
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.creative-tim.com/license">
-                        Licenses
+                    <a href="https://www.google.com">
+                        Example link
                     </a>
                 </li>
             </ul>
